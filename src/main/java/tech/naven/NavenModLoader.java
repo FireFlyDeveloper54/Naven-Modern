c8b9f5f30729b5b6781538a577359c7e8eadbd84
FireFly-Dev <1223010034@qq.com>
2026-03-03 19:29:30 +0800

    b

--- src/main/java/tech/naven/NavenModLoader.java
+++ src/main/java/tech/naven/NavenModLoader.java
@@ -3,10 +3,6 @@ package tech.naven;
 
 import net.fabricmc.api.ModInitializer;
 
-/**
- * @author github.com/AirFoundation
- * @since 26/3/3
- */
 public class NavenModLoader implements ModInitializer {
     @Override
     public void onInitialize() {
